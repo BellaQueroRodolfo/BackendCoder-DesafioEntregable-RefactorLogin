a507bf48bbec40d7b52b8e17efb0efdf4326087c
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-11-28 19:46:09 -0300

    Update User.js

--- src/dao/models/User.js
+++ src/dao/models/User.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: 'usuario' },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user',
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
