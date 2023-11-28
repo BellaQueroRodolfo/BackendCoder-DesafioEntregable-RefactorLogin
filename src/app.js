54f58a4d5b79a427e5e5ab27c660c19d29bf483e
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-11-28 19:47:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,60 +1,90 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
-const mongoose = require('mongoose');
-const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/User');
+const GitHubStrategy = require('passport-github').Strategy;
+const session = require('express-session');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8080;
+const User = require('./src/models/User');
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
+    });
+  })
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: 'your-github-client-id',
+      clientSecret: 'your-github-client-secret',
+      callbackURL: 'http://localhost:8080/auth/github/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      
+      User.findOne({ username: profile.username }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          const newUser = new User({
+            username: profile.username,
+            password: 'generated-password',
+            role: 'user',
+          });
+          newUser.save((err, savedUser) => {
+            if (err) {
+              return done(err);
+            }
+            return done(null, savedUser);
+          });
+        } else {
+          return done(null, user);
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
 
 app.use(express.json());
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, 'src', 'views', 'layouts'),
-  partialsDir: path.join(__dirname, 'src', 'views', 'partials'),
-  extname: '.handlebars',
-}));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'src', 'views'));
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-}, User.authenticate()));
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-mongoose.connect('mongodb+srv://<username>:<password>@cluster.mongodb.net/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-db.once('open', () => {
-  console.log('Connected to MongoDB Atlas');
-});
-
-const productsRouter = require('./src/routes/products');
-const cartsRouter = require('./src/routes/carts');
 const authRouter = require('./src/routes/auth');
-app.use('/products', productsRouter);
-app.use('/carts', cartsRouter);
+
 app.use('/auth', authRouter);
-app.get('/chat', (req, res) => {
-  res.render('chat', { layout: 'main' });
-});
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+
