0cc274c34897169ec07ecf713f40a217a4406ff3
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2024-01-14 19:46:05 -0300

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../dao/models/Product');
+const productRepository = require('../dao/repositories/ProductRepository');
 
 router.get('/', async (req, res) => {
   try {
-    const products = await Product.find();
+    const products = await productRepository.getAllProducts();
     res.json({ products });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
@@ -14,23 +14,12 @@ router.get('/', async (req, res) => {
 router.get('/:pid', async (req, res) => {
   const productId = req.params.pid;
   try {
-    const product = await Product.findById(productId);
+    const product = await productRepository.getProductById(productId);
     if (!product) {
       res.status(404).json({ error: 'Product not found' });
     } else {
       res.json({ product });
     }
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const { title, description, price } = req.body;
-    const newProduct = new Product({ title, description, price });
-    await newProduct.save();
-    res.status(201).json({ product: newProduct });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
