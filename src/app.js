2646adc04e45f41944e5bfe5135b02b23ba6b77b
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2024-01-14 19:58:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,87 +5,37 @@ const LocalStrategy = require('passport-local').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 const session = require('express-session');
 const mongoose = require('mongoose');
-const mockingModule = require('./src/mockingModule'); 
-const customErrorDictionary = require('./src/customErrorDictionary'); 
-const { UserDAO } = require('./src/dao/factory'); 
-const bcrypt = require('bcrypt');
+const mockingModule = require('./src/mockingModule');
+const customErrorDictionary = require('./src/customErrorDictionary');
+const winston = require('winston');
+const logLevels = {
+  debug: 0,
+  http: 1,
+  info: 2,
+  warning: 3,
+  error: 4,
+  fatal: 5,
+};
+
+const developmentLogger = winston.createLogger({
+  levels: logLevels,
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+const productionLogger = winston.createLogger({
+  levels: logLevels,
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'errors.log', level: 'error' }),
+  ],
+});
+
+const logger = process.env.NODE_ENV === 'production' ? productionLogger : developmentLogger;
 
 const app = express();
-const port = 8080;
-
-mongoose.connect('mongodb+srv://<username>:<password>@cluster.mongodb.net/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await UserDAO.findByUsername(username);
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      const isValidPassword = await bcrypt.compare(password, user.password);
-      if (isValidPassword) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-    } catch (error) {
-      return done(error);
-    }
-  })
-);
-
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: 'your-github-client-id',
-      clientSecret: 'your-github-client-secret',
-      callbackURL: 'http://localhost:8080/auth/github/callback',
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const existingUser = await UserDAO.findByUsername(profile.username);
-        if (!existingUser) {
-          const newUser = {
-            username: profile.username,
-            password: 'generated-password',
-            role: 'user',
-          };
-          await UserDAO.createUser(newUser);
-        }
-        const user = await UserDAO.findByUsername(profile.username);
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await UserDAO.findById(id);
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
 
 app.use(express.json());
 app.use(
@@ -108,9 +58,20 @@ app.get('/mockingproducts', (req, res) => {
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = customErrorDictionary[err.type] || 'Internal Server Error';
+  logger.error(`[${status}] ${message}`);
+  
   res.status(status).json({ error: message });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get('/loggerTest', (req, res) => {
+  logger.debug('This is a debug message');
+  logger.http('This is an http message');
+  logger.info('This is an info message');
+  logger.warning('This is a warning message');
+  logger.error('This is an error message');
+  logger.fatal('This is a fatal message');
+
+  res.send('Logger test complete. Check the console or log files.');
 });
+
+module.exports = app;
